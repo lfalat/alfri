8fa4d3cd9d08453036acbf26c0f93ab633407ca6
Peter Szathmary <peter.szathmary01@gmail.com>
2024-04-16 19:59:28 +0200

    BE - main class renamed

--- BE/src/main/java/sk/uniza/fri/alfri/AlfriApplication.java
+++ BE/src/main/java/sk/uniza/fri/alfri/AlfriApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BeApplication {
+public class AlfriApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BeApplication.class, args);
+        SpringApplication.run(AlfriApplication.class, args);
     }
 
 }
