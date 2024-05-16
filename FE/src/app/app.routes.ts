b308b35f383fed433de2fdc9b1b23f8e84f6a4bb
Peter Szathmary <peter.szathmary01@gmail.com>
2024-05-16 13:12:29 +0200

    eslint errors fixed

--- FE/src/app/app.routes.ts
+++ FE/src/app/app.routes.ts
@@ -4,10 +4,11 @@ import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { AuthGuard } from './auth-guard';
 import { ErrorPageComponent } from './error-page/error-page.component';
+import { inject } from '@angular/core';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'home', component: HomeComponent, canActivate: [() => inject(AuthGuard).canActivate()] },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegistrationComponent },
   { path: '404', component: ErrorPageComponent },
