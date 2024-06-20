008989ce0da38c66552d6c4ec967526e2cb00f32
Adam Nagy <aduxik@gmail.com>
2024-06-20 22:36:30 +0200

    docker working

--- BE/src/main/java/sk/uniza/fri/alfri/AlfriApplication.java
+++ BE/src/main/java/sk/uniza/fri/alfri/AlfriApplication.java
@@ -2,13 +2,11 @@ package sk.uniza.fri.alfri;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
 public class AlfriApplication {
 
   public static void main(String[] args) {
-    System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "production");
     SpringApplication.run(AlfriApplication.class, args);
   }
 }
