646bac0fe2e7fc33f1984d3c1b8445cd36d82662
Peter Szathmary <peter.szathmary01@gmail.com>
2024-09-20 20:57:16 +0200

    feature(testing): Basic tests with test profile and h2 database implemented

--- BE/src/test/java/sk/uniza/fri/alfri/BeApplicationTests.java
+++ BE/src/test/java/sk/uniza/fri/alfri/BeApplicationTests.java
@@ -4,8 +4,10 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BeApplicationTests {
 
   @Test
