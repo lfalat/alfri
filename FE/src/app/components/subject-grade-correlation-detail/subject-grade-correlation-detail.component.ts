d0dd55026ac558563f06e9ecebd00dfea35597dd
Peter Szathmary <peter.szathmary01@gmail.com>
2024-11-09 16:14:50 +0100

    feat(SubjectGradeCorrelation): Detailed views of subject grade correlation done

---
+++ FE/src/app/components/subject-grade-correlation-detail/subject-grade-correlation-detail.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input } from '@angular/core';
+import { MatCard, MatCardContent, MatCardHeader, MatCardTitle } from '@angular/material/card';
+import { SubjectGradeCorrelation } from '../../types';
+import { DecimalPipe, NgIf, PercentPipe } from '@angular/common';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-subject-grade-correlation-detail',
+  standalone: true,
+  imports: [
+    MatCard,
+    MatCardContent,
+    MatCardHeader,
+    MatCardTitle,
+    PercentPipe,
+    NgIf,
+    DecimalPipe
+  ],
+  templateUrl: './subject-grade-correlation-detail.component.html',
+  styleUrl: './subject-grade-correlation-detail.component.scss'
+})
+export class SubjectGradeCorrelationDetailComponent {
+  @Input() correlationData: SubjectGradeCorrelation | undefined;
+
+  constructor(private readonly router: Router) {
+  }
+
+
+  navigateToSubjectDetail(code: string) {
+    this.router.navigate(['/subjects/' + code]);
+  }
+}
