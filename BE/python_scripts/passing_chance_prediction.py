6bef3b7f07101365179aca3063c20201d728a67f
majbam <majbam@posam.sk>
2025-02-14 01:40:55 +0100

    finsih ?

--- BE/python_scripts/passing_chance_prediction.py
+++ BE/python_scripts/passing_chance_prediction.py
@@ -2,8 +2,8 @@ import os
 import warnings
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-warnings.filterwarnings("ignore", category=UserWarning)  # Suppress user warnings
-warnings.filterwarnings("ignore", category=FutureWarning)  # Suppress future warnings
+warnings.filterwarnings('ignore')
+
 
 import json
 import sys
