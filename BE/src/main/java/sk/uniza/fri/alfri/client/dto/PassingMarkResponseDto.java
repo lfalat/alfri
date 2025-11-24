f821aa52402f01a22fc031c3167bd85a6cb29dda
klacek <maros.majba2551@gmail.com>
2025-11-25 00:25:00 +0100

    working neural networks

--- BE/src/main/java/sk/uniza/fri/alfri/client/dto/PassingMarkResponseDto.java
+++ BE/src/main/java/sk/uniza/fri/alfri/client/dto/PassingMarkResponseDto.java
@@ -1,34 +1,15 @@
 package sk.uniza.fri.alfri.client.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Map;
 
+@Setter
+@Getter
 public class PassingMarkResponseDto {
     private String subject;
     private Map<String, Double> distribution;
-    private String chosen_grade;
-
-    public String getSubject() {
-        return subject;
-    }
-
-    public void setSubject(String subject) {
-        this.subject = subject;
-    }
-
-    public Map<String, Double> getDistribution() {
-        return distribution;
-    }
-
-    public void setDistribution(Map<String, Double> distribution) {
-        this.distribution = distribution;
-    }
-
-    public String getChosen_grade() {
-        return chosen_grade;
-    }
-
-    public void setChosen_grade(String chosen_grade) {
-        this.chosen_grade = chosen_grade;
-    }
+    private String chosenGrade;
 }
 
