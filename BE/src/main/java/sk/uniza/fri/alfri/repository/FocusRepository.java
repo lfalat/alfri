e3aeb7c5d2831877379108192eb09a784a085fb9
majbam <majbam@posam.sk>
2026-02-15 20:55:18 +0100

    VIP changes

--- BE/src/main/java/sk/uniza/fri/alfri/repository/FocusRepository.java
+++ BE/src/main/java/sk/uniza/fri/alfri/repository/FocusRepository.java
@@ -12,22 +12,25 @@ import sk.uniza.fri.alfri.entity.Subject;
 import java.util.List;
 
 public interface FocusRepository extends JpaRepository<Focus, Integer> {
-    @Query("SELECT DISTINCT f.subject FROM Focus f "
-            + "JOIN f.subject.studyProgramSubjects sps "
-            + "WHERE ((COALESCE(:mathFocus, 0) > 5 AND f.mathFocus > 5) OR "
-            + "(COALESCE(:logicFocus, 0) > 5 AND f.logicFocus > 5) OR "
-            + "(COALESCE(:programmingFocus, 0) > 5 AND f.programmingFocus > 5) OR "
-            + "(COALESCE(:designFocus, 0) > 5 AND f.designFocus > 5) OR "
-            + "(COALESCE(:economicsFocus, 0) > 5 AND f.economicsFocus > 5) OR "
-            + "(COALESCE(:managementFocus, 0) > 5 AND f.managementFocus > 5) OR "
-            + "(COALESCE(:hardwareFocus, 0) > 5 AND f.hardwareFocus > 5) OR "
-            + "(COALESCE(:networkFocus, 0) > 5 AND f.networkFocus > 5) OR "
-            + "(COALESCE(:dataFocus, 0) > 5 AND f.dataFocus > 5) OR "
-            + "(COALESCE(:testingFocus, 0) > 5 AND f.testingFocus > 5) OR "
-            + "(COALESCE(:languageFocus, 0) > 5 AND f.languageFocus > 5) OR "
-            + "(COALESCE(:physicalFocus, 0) > 5 AND f.physicalFocus > 5)) "
-            + "AND sps.studyProgram.id = :studyProgramId "
-            + "AND sps.recommendedYear >= :year")
+    @Query("""
+            SELECT DISTINCT f.subject FROM Focus f
+            JOIN f.subject.studyProgramSubjects sps
+            WHERE ((COALESCE(:mathFocus, 0) > 5 AND f.mathFocus > 5) OR
+                   (COALESCE(:logicFocus, 0) > 5 AND f.logicFocus > 5) OR
+                   (COALESCE(:programmingFocus, 0) > 5 AND f.programmingFocus > 5) OR
+                   (COALESCE(:designFocus, 0) > 5 AND f.designFocus > 5) OR
+                   (COALESCE(:economicsFocus, 0) > 5 AND f.economicsFocus > 5) OR
+                   (COALESCE(:managementFocus, 0) > 5 AND f.managementFocus > 5) OR
+                   (COALESCE(:hardwareFocus, 0) > 5 AND f.hardwareFocus > 5) OR
+                   (COALESCE(:networkFocus, 0) > 5 AND f.networkFocus > 5) OR
+                   (COALESCE(:dataFocus, 0) > 5 AND f.dataFocus > 5) OR
+                   (COALESCE(:testingFocus, 0) > 5 AND f.testingFocus > 5) OR
+                   (COALESCE(:languageFocus, 0) > 5 AND f.languageFocus > 5) OR
+                   (COALESCE(:physicalFocus, 0) > 5 AND f.physicalFocus > 5))
+            AND sps.studyProgram.id = :studyProgramId
+            AND sps.recommendedYear >= :year
+            AND sps.obligation != 'Pov.'
+            """)
     Page<Subject> findSubjectByHashMapValuesWithPaging(@Param("mathFocus") Integer mathFocus,
                                                        @Param("logicFocus") Integer logicFocus, @Param("programmingFocus") Integer programmingFocus,
                                                        @Param("designFocus") Integer designFocus, @Param("economicsFocus") Integer economicsFocus,
